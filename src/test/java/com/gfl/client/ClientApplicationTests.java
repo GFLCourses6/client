42ecf8440748fd46dadcb816cf93602350a209f7
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-01-07 11:56:52 +0200

    Initial Commit

---
+++ src/test/java/com/gfl/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfl.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
