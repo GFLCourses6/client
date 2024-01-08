7f073ac1b64e1292eb94e07158a047d0e1238cdb
Mazurenko Nikita <104276704+MazurenkoNick@users.noreply.github.com>
2024-01-08 18:38:43 +0200

    Create custom JwtAuthConverter (#3)

--- src/main/java/com/gfl/client/config/SecurityConfiguration.java
+++ src/main/java/com/gfl/client/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.gfl.client.config;
 
+import com.gfl.client.security.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,16 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(
+                                jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)
+                        )
+                );
         return http.build();
     }
 }
--- src/main/java/com/gfl/client/controller/ProxySourceController.java
+++ src/main/java/com/gfl/client/controller/ProxySourceController.java
@@ -3,6 +3,7 @@ package com.gfl.client.controller;
 import com.gfl.client.model.ProxyCredentialsRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProxySourceController {
 
     @PostMapping
+    @PreAuthorize("hasRole('user')")
     public ResponseEntity<ProxyCredentialsRequest> addProxy(
             @RequestBody @Valid ProxyCredentialsRequest proxy) {
         // todo: implement logic to add proxy
