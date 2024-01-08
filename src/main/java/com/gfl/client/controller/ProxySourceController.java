bed54812d735abfc166332e4e40f8c5efe873f50
Pasha <78595276+Lokankara@users.noreply.github.com>
2024-01-08 18:34:35 +0200

    Created Scenario Http service (#2)
    * Created Scenario Http service
     - added controller
     - request dtos: Scenario, Step, Proxy
     - created tests for controller, service, model
    
    * Removed Scenario Controller
    Added Error handler
    Modified Scenario Service

--- src/main/java/com/gfl/client/controller/ProxySourceController.java
+++ src/main/java/com/gfl/client/controller/ProxySourceController.java
@@ -1,18 +1,23 @@
 package com.gfl.client.controller;
 
-import org.springframework.http.HttpStatus;
+import com.gfl.client.model.ProxyCredentialsRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/proxy")
 public class ProxySourceController {
 
     @PostMapping
-    public ResponseEntity<?> addProxy() {
+    public ResponseEntity<ProxyCredentialsRequest> addProxy(
+            @RequestBody @Valid ProxyCredentialsRequest proxy) {
         // todo: implement logic to add proxy
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return ResponseEntity.ok(proxy);
     }
 }
