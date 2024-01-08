073db621c8008ec496fe744bb6e7744ce9a270f9
williambrownqqq <alexey.zanchenko@gmail.com>
2024-01-08 20:22:19 +0200

    implement ProxySourceService (refactor)

--- src/main/java/com/gfl/client/model/dto/ProxyConfigHolder.java
+++ src/main/java/com/gfl/client/model/dto/ProxyConfigHolder.java
@@ -1,14 +1,10 @@
 package com.gfl.client.model.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ProxyConfigHolder{
-    private ProxyNetworkConfig proxyNetworkConfig;
-    private ProxyCredentials proxyCredentials;
+    private final ProxyNetworkConfig proxyNetworkConfig;
+    private final ProxyCredentials proxyCredentials;
 
 }
