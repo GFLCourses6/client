c718fdce453e7a2d55175ceab6cfcdb02cf424b6
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-01-09 14:07:26 +0200

    Constructor clean-up & add ProxyConfigs.json

--- src/main/java/com/gfl/client/service/proxy/ProxySourceServiceFile.java
+++ src/main/java/com/gfl/client/service/proxy/ProxySourceServiceFile.java
@@ -4,27 +4,25 @@ package com.gfl.client.service.proxy;
 import com.gfl.client.exception.FileReadException;
 import com.gfl.client.model.ProxyConfigHolder;
 import com.gfl.client.util.file.FileParser;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProxySourceServiceFile implements ProxySourceService {
 
-    private static final String PROXY_CONFIGS_PATH = "json/ProxyConfigs.json";
+    @Value("${proxy.filepath}")
+    private String proxyFilePath;
     private final FileParser fileParser;
-
-    @Autowired
-    public ProxySourceServiceFile(FileParser fileParser) {
-        this.fileParser = fileParser;
-    }
 
     @Override
     public List<ProxyConfigHolder> getAllProxyConfigs(){
         try {
-            return fileParser.getAllFromFile(PROXY_CONFIGS_PATH, ProxyConfigHolder.class);
+            return fileParser.getAllFromFile(proxyFilePath, ProxyConfigHolder.class);
         } catch (IOException e){
             throw new FileReadException(e.getMessage());
         }
