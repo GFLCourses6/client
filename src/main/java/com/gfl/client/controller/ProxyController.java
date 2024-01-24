a9ffc6b79e441c19ffee395adc0284c22a8d7259
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-01-24 16:44:08 +0200

    Move API docs to interfaces

---
+++ src/main/java/com/gfl/client/controller/ProxyController.java
@@ -0,0 +1,45 @@
+package com.gfl.client.controller;
+
+import com.gfl.client.model.ProxyConfigHolder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+
+@Api(value = "Operations pertaining to proxy sources")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Invalid input provided"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized"),
+        @ApiResponse(responseCode = "403", description = "Forbidden"),
+        @ApiResponse(responseCode = "404", description = "Proxy configuration not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+})
+public interface ProxyController {
+
+    @ApiOperation(value = "Add a proxy configuration",
+            notes = "Endpoint for adding a proxy configuration. Requires authentication.",
+            response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully added the proxy"),
+    })
+    ResponseEntity<Void> addProxy(
+            Authentication authentication,
+            @ApiParam(value = "Proxy config holder object", required = true)
+            @Valid ProxyConfigHolder proxy);
+
+    @ApiOperation(value = "Get proxy configuration by username",
+            notes = "Endpoint for retrieving a proxy configuration by username. Requires 'WORKER' role.",
+            response = ProxyConfigHolder.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Proxy configuration retrieved successfully"),
+    })
+    ResponseEntity<ProxyConfigHolder> getProxy(
+            @ApiParam(value = "Username for which proxy needs to be retrieved", required = true)
+            String username);
+
+}
+
--- src/main/java/com/gfl/client/controller/ScenarioSourceController.java
+++ src/main/java/com/gfl/client/controller/ScenarioSourceController.java
@@ -3,11 +3,6 @@ package com.gfl.client.controller;
 import com.gfl.client.model.ScenarioRequest;
 import com.gfl.client.model.ScenarioResult;
 import com.gfl.client.service.scenario.RestTemplateScenarioService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -26,28 +21,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/scenario")
-@Api(value = "Operations pertaining to scenario sources")
-@ApiResponses(value = {
-        @ApiResponse(responseCode = "400", description = "Invalid input provided"),
-        @ApiResponse(responseCode = "401", description = "Unauthorized"),
-        @ApiResponse(responseCode = "403", description = "Forbidden"),
-        @ApiResponse(responseCode = "404", description = "Executed scenarios not found"),
-        @ApiResponse(responseCode = "405", description = "Method not allowed"),
-        @ApiResponse(responseCode = "500", description = "Internal server error")
-})
-public class ScenarioSourceController {
+public class ScenarioSourceController implements ScenarioController {
 
     private final RestTemplateScenarioService restTemplateScenarioService;
 
     private final Logger logger = LoggerFactory.getLogger(ScenarioSourceController.class);
 
-    @ApiOperation(value = "Send scenarios request")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Scenarios sent successfully"),
-    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> sendScenariosRequest(
-            @ApiParam(value = "List of scenario requests", required = true)
             @RequestBody @NotEmpty(message = "scenarioRequest list can not be empty")
             List<@Valid ScenarioRequest> scenarios) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -55,43 +36,26 @@ public class ScenarioSourceController {
         return restTemplateScenarioService.sendScenarios(username, scenarios);
     }
 
-    @ApiOperation(value = "Get executed scenarios")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Scenarios retrieved successfully")
-    })
     @GetMapping(value = "/executed/{username}")
     public ResponseEntity<List<ScenarioResult>> getExecutedScenarios(
-            @ApiParam(value = "Username for which executed scenarios need to be retrieved", required = true)
             @NotBlank(message = "username can't be blank")
-            @PathVariable("username") String username) {
+            @PathVariable String username) {
         return restTemplateScenarioService.getExecutedScenarios(username);
     }
 
-    @ApiOperation(value = "Get scenarios from queue by username")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Executed scenarios retrieved successfully")
-    })
     @GetMapping(value = "/queue/{username}")
     public ResponseEntity<List<ScenarioRequest>> getScenariosFromQueueByUsername(
-            @ApiParam(value = "Username for which scenarios need to be retrieved from queue", required = true)
             @NotBlank(message = "username can't be blank")
-            @PathVariable("username") String username) {
+            @PathVariable String username) {
         return restTemplateScenarioService.getScenariosFromQueue(username);
     }
 
-    @ApiOperation(value = "Get scenarios from queue by username and scenario name")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Scenarios retrieved successfully from queue")
-    })
     @GetMapping(value = "/queue/{username}/{scenarioName}")
-
     public ResponseEntity<List<ScenarioRequest>> getScenariosFromQueueByUsernameAndScenarioName(
-            @ApiParam(value = "Username for which scenarios need to be retrieved from queue", required = true)
             @NotBlank(message = "username can't be blank")
-            @PathVariable("username") String username,
-            @ApiParam(value = "Scenario name for which scenarios need to be retrieved from queue", required = true)
+            @PathVariable String username,
             @NotBlank(message = "scenarioName can't be blank")
-            @PathVariable("scenarioName") String scenarioName) {
+            @PathVariable String scenarioName) {
         return restTemplateScenarioService.getScenariosFromQueue(username, scenarioName);
     }
 }
