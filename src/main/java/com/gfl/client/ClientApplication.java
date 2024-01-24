0512c8f0fc6e4fd180eda8b5fe3b2eff51ce0cb5
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-01-24 13:07:46 +0200

    Move CommandLineRunner to a new class

--- src/main/java/com/gfl/client/ClientApplication.java
+++ src/main/java/com/gfl/client/ClientApplication.java
@@ -1,8 +1,5 @@
 package com.gfl.client;
 
-import com.gfl.client.service.proxy.queue.AsyncProxyQueueTaskProcessor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,17 +8,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@RequiredArgsConstructor
-public class ClientApplication implements CommandLineRunner {
-
-    private final AsyncProxyQueueTaskProcessor proxyQueueTaskProcessor;
+public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        proxyQueueTaskProcessor.fillCommonQueue();
     }
 }
