adf6fb8a276476c41c751d6fbb77b99af8e977fa
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-01-25 11:31:50 +0200

    Add tests for ProxySourceServiceUrl

--- src/main/java/com/gfl/client/service/proxy/source/ProxySourceService.java
+++ src/main/java/com/gfl/client/service/proxy/source/ProxySourceService.java
@@ -5,5 +5,5 @@ import com.gfl.client.model.ProxyConfigHolder;
 import java.util.List;
 
 public interface ProxySourceService {
-        List<ProxyConfigHolder> getAllProxyConfigs();
+    List<ProxyConfigHolder> getAllProxyConfigs();
 }
